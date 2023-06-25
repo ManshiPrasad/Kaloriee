7eb824cb215754f9667611e05668bf4757fa9f39
Manshi <manshiprasad60@gmail.com>
2023-06-25 16:25:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+function Calo()
+{
+   var h=document.getElementById('hi').value;
+   var w=document.getElementById('we').value;
+   var age=document.getElementById('age').value;
+
+   var calo=(10* w/1 + 6.25 * h/1 - 5 * age/1 + 5);
+   var calom = calo * 1.4;
+
+   document.getElementById("resultcal").innerHTML="Your Daily Calories intake: " +calom;
+
+
+   // for showing protein fats carbs in pie chart
+
+   const protein = calom *0.12;
+   const carbs = calom * 0.60;
+   const fat = calom * 0.27;
+
+let data2 = [protein, fat, carbs ];
+const ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx,{
+    type: 'doughnut',
+    data: {
+        labels: ['Protein', 'Fat', 'Carbs' ],
+        datasets : [
+            {
+                label : '# of votes',
+                data : data2,
+                backgroundColor : ['#2adece', '#dd3b79' , '#ff766b'],
+                borderwidth: 1
+            }
+        ]
+    }
+});
+}
+
+
+
+function Clear()
+{
+    document.getElementById('resultcal').innerHTML="";
+}
